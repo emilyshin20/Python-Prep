b506a576ec0d00e1bbdd02d34fb579a010913b87
Emily Shin <emilyshin20@gmail>
2023-02-28 15:53:01 -0300

    first commit

--- 02 - Variables y Tipos de Datos/Practica en Clase.py
+++ 02 - Variables y Tipos de Datos/Practica en Clase.py
@@ -1,4 +1,4 @@
-mi_variable = 12
+mi_variable = 123
 print(mi_variable)
 
 mi_variable2 = 'dario'
@@ -6,7 +6,6 @@ print(mi_variable2)
 
 mi_complejo = 5 + 7j
 print(mi_complejo)
-
 
 mi_numero = input('Ingrese un valor')
 print('el valor ingresado fue', mi_numero)
